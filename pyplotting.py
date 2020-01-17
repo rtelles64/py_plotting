a9513bba194711b4e96bf87a0179b5773eab8f55
Roy Telles <x-rtelles@4353-laptop.local>
2020-01-17 14:22:51 -0800

    Add project file with preliminary info

---
+++ pyplotting.py
@@ -0,0 +1,9 @@
+# PYTHON PLOTTING WITH MATPLOTLIB (GUIDE)
+# https://realpython.com/python-matplotlib-guide/
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(444)
+
+# THE MATPLOTLIB OBJECT HIERARCHY
